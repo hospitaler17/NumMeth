d0dd2d8ed17b7a1c9792aa1e61b983ebaf3935cf
Dmitrii <hospitaler17@gmail.com>
2017-11-07 00:05:02 +0300

    Попытка отправить через атом 2

--- pit/main.cpp
+++ pit/main.cpp
@@ -1,5 +1,15 @@
-#include <>}
-        }
+#include <iostream>
+
+
+int main (int argc, char** argv) {
+long double X,Y; // х и у для обработки
+long double arrX, arrY; // массив для хранения точек
+char* name;// имя входного файла
+if(argc < 1) {
+
+			std::cerr << "Ошибка аргумента!\nТип ввода аргуметов: ./[prog_name] [input_file.txt]\n";
+			return 1;
+}
 	name = argv[1];
-	
+
 }
