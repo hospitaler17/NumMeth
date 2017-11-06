5fc9feb3e3b3f4dc4850612124a533b40a03bfa5
Dmitrii <hospitaler17@gmail.com>
2017-11-07 01:08:22 +0300

    просто дополнил

--- pit/main.cpp
+++ pit/main.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <fstream>
 
+using namespace std;
 
 int main (int argc, char** argv) {
-long double X,Y; // х и у для обработки
-long double arrX, arrY; // массив для хранения точек
-char* name;// имя входного файла
-if(argc < 1) {
+								long double X,Y; // х и у для обработки
+								long double arrX, arrY; // массив для хранения точек
+								char* name; // имя входного файла
+								fstream f; // файл
+								if (argc < 2) {
 
-			std::cerr << "Ошибка аргумента!\nТип ввода аргуметов: ./[prog_name] [input_file.txt]\n";
-			return 1;
-}
-	name = argv[1];
+																cerr << "Ошибка аргумента!\nТип ввода аргуметов: ./[prog_name] [input_file.txt]\n";
+																return 0;
+								}
+								else {
+																name = argv[1];
+								}
+								f.open (name, ios_base::in); // блок открытия файла
+								if (!f.is_open()) { // проверка файла
+																cerr << "Не удалось открыть файл!\n";
+																return 0;
+								}
+								else
+																cout << "Файл "<< name << " успешно открыт!" << '\n';
 
 }
