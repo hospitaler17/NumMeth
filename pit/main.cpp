c7c3f5dd3407dcda92009f2053fc49050d6883d3
Dmitrii <hospitaler17@gmail.com>
2017-11-07 16:00:26 +0300

    Не работает считывание из файла в массив

--- pit/main.cpp
+++ pit/main.cpp
@@ -3,31 +3,63 @@
 /* y = (Ax+C)/B */
 
 /*==============================================================================*/
-
 #include <iostream>
 #include <fstream>
+
+#define  N 100
+
 
 using namespace std;
 
 int main (int argc, char** argv) {
-	long double X,Y; // х и у для обработки
-	long double arrX, arrY; // массив для хранения точек
-	char* name; // имя входного файла
+	int i, j, count = 0;// счетчики
+	long double X, Y; // х и у для обработки
+	long double arrX[N], arrY[N];
+	char name[] = "dots.txt"; // имя входного файла
 	fstream f; // файл
-	if (argc < 2) {
-		cerr << "Ошибка аргумента!\nТип ввода аргуметов: ./[prog_name] [input_file.txt]\n";
-		return 0;
-	}
-	else {
-		name = argv[1];
-	}
 	f.open (name, ios_base::in); // блок открытия файла
 	if (!f.is_open()) { // проверка файла
 		cerr << "Не удалось открыть файл!\n";
 		return 0;
 	}
 	else
-	cout << "Файл "<< name << " успешно открыт!" << '\n';
+		cout << "Файл "<< name << " успешно открыт!" << '\n';
+	while (!f.eof()) {
+		f >> arrX[count];
+		f >> arrY[count];
+		count++;
+		std::cout << "Найдена точка ( " <<arrX[count]<< " \t, "<< arrY[count] <<" )\n";
+	}
+	count--;// из-за перехода на след строку(
+	std::cout << "Всего точек "<< count << '\n';
+	//f.clear();// отчистим поток
+	//f.seekg(0);// двинемся в начало файла
+	f.close();
+	//long double *arrX = new long double[i];// массив для хранения точек
+	//long double *arrY = new long double[i];
+	for (i = 0; i < count; i++)
+		std::cout << "Найдена точка ( " <<X<< " \t, "<<Y<<" )\n";
+	std::cout << "Точки загружены в память" << '\n';
+	while (true) { // сортировка
+		i=0;
+		for (j = 0; j < count; j++) {
+			if(arrX[j] > arrX[j+1]) {
+				X = arrX[j];
+				Y = arrY[j];
+				arrX[j] = arrX[j+1];
+				arrY[j] = arrY[j+1];
+				arrX[j+1] = X;
+				arrY[j+1] = Y;
+				i=1;
+			}
+		}
+		if(i == 0)
+			break;
+	}
+	for (i = 0; i < count; i++) {
+		std::cout <<i+1<< ": ( " << X << " \t, "<<Y<<" )\n";
+	}
+	//delete[] arrX, arrY;
 
 }
 //TODO: сделать анализатор для чисел из файла
@@ -35,5 +67,3 @@ int main (int argc, char** argv) {
 //TODO: Написать меню
 //TODO: разобратся с графиком
 //TODO: написать функцию-сортировку по Х
-//TODO: *поиск по половинке
-//TODO: количество X = Y
